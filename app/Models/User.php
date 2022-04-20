ffafd12a26559d3804320489f85ee12d52bae51c
Álvaro <templealvaro@gmail.com>
2022-04-20 19:23:55 +0200

    [dev] Created Controller & Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function component() {
+        return $this->hasMany(Component::class);
+    }
 }
